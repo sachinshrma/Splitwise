90bc542aa58a046afeb468a0d2d910fe20687d78
Sachin Sharma <sachinsharma9998@gmail.com>
2024-09-21 22:38:36 +0530

    Optimisation

--- src/service/BalanceService.java
+++ src/service/BalanceService.java
@@ -25,41 +25,36 @@ public class BalanceService {
 
             if (!paidBy.equals(taker)) {
                 Map<String, Double> balances = userBalances.getOrDefault(taker.getId(), new HashMap<>());
-                String key = getBalanceKey(taker, paidBy);
-                balances.put(key, balances.getOrDefault(key, 0d) - split.getValue());
+                balances.put(paidBy.getId(), balances.getOrDefault(paidBy.getId(), 0d) - split.getValue());
                 userBalances.put(taker.getId(), balances);
 
                 balances = userBalances.getOrDefault(paidBy.getId(), new HashMap<>());
-                key = getBalanceKey(paidBy, taker);
-                balances.put(key, balances.getOrDefault(key, 0d) + split.getValue());
+                balances.put(taker.getId(), balances.getOrDefault(taker.getId(), 0d) + split.getValue());
                 userBalances.put(paidBy.getId(), balances);
             }
         }
-    }
-
-    private String getBalanceKey(User user1, User user2) {
-        return user1.getId() + ":" + user2.getId();
     }
 
     public void showBalance(User user) {
         System.out.println(user.getName() + " Balance :" );
         Map<String, Double> balances = userBalances.getOrDefault(user.getId(), new HashMap<>());
         for (Map.Entry<String, Double> entry: balances.entrySet()) {
-            System.out.println(userService.getUser(entry.getKey().split(":")[1]).getName() + " -- " + entry.getValue());
+            User balanceUser = userService.getUser(entry.getKey());
+            if (balanceUser != null) {
+                System.out.println(balanceUser.getName() + " : " + entry.getValue());
+            }
         }
     }
 
     public void settleBalance(User user1, User user2) {
-        String key = getBalanceKey(user1, user2);
-        double balance = userBalances.getOrDefault(user1.getId(), new HashMap<>()).getOrDefault(key, 0d);
+        double balance = userBalances.getOrDefault(user1.getId(), new HashMap<>()).getOrDefault(user2.getId(), 0d);
         if (balance != 0) {
-            userBalances.get(user1.getId()).remove(key);
+            userBalances.get(user1.getId()).remove(user2.getId());
         }
 
-        key = getBalanceKey(user2, user1);
-        balance = userBalances.getOrDefault(user2.getId(), new HashMap<>()).getOrDefault(key, 0d);
+        balance = userBalances.getOrDefault(user2.getId(), new HashMap<>()).getOrDefault(user1.getId(), 0d);
         if (balance != 0) {
-            userBalances.get(user2.getId()).remove(key);
+            userBalances.get(user2.getId()).remove(user1.getId());
         }
     }
 }
